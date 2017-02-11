9fbbb2d65897b7170f0670b7d761c758808ba579
John Grosh <john.a.grosh@gmail.com>
2017-02-11 12:48:38 -0500

    Update frost.py

--- frost.py
+++ frost.py
@@ -14,7 +14,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if not message.author.bot and (message.server == None or client.user in message.mentions):
+    if not message.author.bot and (message.server == None or client.user in message.mentions):
         await client.send_typing(message.channel)
         txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
         r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'frost', 'text':txt}).text)
