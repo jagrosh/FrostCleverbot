cbfbc275fe301522f878b9c819854b06abaf1874
M R S E <Maurice.Craft@t-online.de>
2019-06-11 22:44:35 +0200

    discord.py 1.2.2 update
    including latest discord.py changes

--- frost.py
+++ frost.py
@@ -2,12 +2,12 @@ import asyncio, aiohttp, discord, os, re
 
 class FrostCleverbot(discord.Client):
     async def on_ready(self):
-        print('Logged in as {0.user.name} (ID: {0.user.id} ) | {1} servers'.format(self, str(len(self.servers))))
-        await self.change_presence(game=discord.Game(name='chat with me!'))
+        print('Logged in as {0.user.name} (ID: {0.user.id} ) | {1} servers'.format(self, str(len(self.guilds))))
+        await self.change_presence(status=discord.Status.online, activity=discord.Game("chat with me!"))
 
     async def on_message(self, message):
-        if not message.author.bot and (not message.server or message.server.me in message.mentions):
-            await self.send_typing(message.channel)
+        if not message.author.bot and (not message.guild or message.guild.me in message.mentions):
+            await message.channel.typing()
             try:
                 input = re.sub('<@!?'+self.user.id+'>', '', message.content).strip()
                 params = {'botid': os.environ['PANDORA_BOT'] or 'PANDORABOTS BOT ID HERE', 'custid': message.author.id, 'input': input or 'Hello'}
@@ -16,11 +16,11 @@ class FrostCleverbot(discord.Client):
                         text = await resp.text()
                         text = text[text.find('<that>')+6:text.rfind('</that>')]
                         text = text.replace('&quot;','"').replace('&lt;','<').replace('&gt;','>').replace('&amp;','&').replace('<br>',' ')
-                        await self.send_message(message.channel, text)
+                        await message.channel.send(text)
                     else:
-                        await self.send_message(message.channel, 'Uh oh, I didn\'t quite catch that!')
+                        await message.channel.send('Uh oh, I didn\'t quite catch that!')
             except asyncio.TimeoutError:
-                await self.send_message(message.channel, 'Uh oh, I think my head is on backwards!')
+                await message.channel.send('Uh oh, I think my head is on backwards!')
 
 print('Starting...')
 http = aiohttp.ClientSession()
