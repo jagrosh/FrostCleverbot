66481cc7077138eae725765a265404fbabb258b9
Sanduhr32 <maurice.craft@t-online.de>
2019-06-12 14:02:55 +0200

    Python 1.2.2

--- frost.py
+++ frost.py
@@ -2,14 +2,14 @@ import asyncio, aiohttp, discord, os, re
 
 class FrostCleverbot(discord.Client):
     async def on_ready(self):
-        print('Logged in as {0.user.name} (ID: {0.user.id} ) | {1} servers'.format(self, str(len(self.guilds))))
+        print('Logged in as {0.user.name} (ID: {0.user.id}) | {1} servers'.format(self, str(len(self.guilds))))
         await self.change_presence(status=discord.Status.online, activity=discord.Game("chat with me!"))
 
     async def on_message(self, message):
         if not message.author.bot and (not message.guild or message.guild.me in message.mentions):
-            await message.channel.typing()
+            async with message.channel.typing():
             try:
-                input = re.sub('<@!?'+self.user.id+'>', '', message.content).strip()
+                input = re.sub('<@!?'+str(self.user.id)+'>', '', message.content).strip()
                 params = {'botid': os.environ['PANDORA_BOT'] or 'PANDORABOTS BOT ID HERE', 'custid': message.author.id, 'input': input or 'Hello'}
                 async with http.get('https://www.pandorabots.com/pandora/talk-xml', params=params) as resp:
                     if resp.status == 200:
